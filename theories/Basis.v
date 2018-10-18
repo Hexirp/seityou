18192560590f2bae3ffe71b982d40520869401a9
Hexirp <Hexirp@users.noreply.github.com>
2018-10-18 18:15:32 +0900

    Add: 一階述語論理に対応するものを一通り
    "x -> y" は標準ライブラリからの写しである。[empty] は恒偽である命題、
    空集合、始対象、要素数が負の無限大のタプル、矛盾、ボトム型、その他、
    様々な意味合いを持つ型である。[unit] は、恒真である命題、一点集合、
    終対象、要素数ゼロのタプル、直積の単位元、void 型、恒真式、その他、
    様々な意味合いを持つ型である。[sum] は、直和型である。[prod] は、
    product と書き、直積型である。[dsum] は dependent sum と書き、依存
    直和型である。[dprod] は dependent product と書き、依存直積型である。
    以上の７つの型で一階述語論理を表すことが出来る。
    
    分かりやすくするためにコンストラクタ、定義はこのように字下げする。

--- theories/Basis.v
+++ theories/Basis.v
@@ -1,3 +1,33 @@
 Unset Elimination Schemes.
 
-Inductive unit : Type := tt : unit.
+Notation "x -> y" := (forall (_ : x), y)
+  (at level 99, right associativity, y at level 200)
+  .
+
+Inductive empty : Type
+  :=
+  .
+
+Inductive unit : Type
+  :=
+  | tt : unit
+  .
+
+Inductive sum (A : Type) (B : Type) : Type
+  :=
+  | left : A -> sum A B
+  | right : B -> sum A B
+  .
+
+Inductive prod (A : Type) (B : Type) : Type
+  :=
+  | pair : A -> B -> prod A B
+  .
+
+Inductive dsum (A : Type) (B : A -> Type) : Type
+  :=
+  | evi : forall x : A, B x -> dsum A B
+  .
+
+Definition dprod (A : Type) (B : A -> Type) : Type
+  := forall x : A, B x .
