1d89186ae875dbfb195d8a97f99fe7709bf212bc
Hexirp <Hexirp@users.noreply.github.com>
2018-10-21 10:44:01 +0900

    Add: 除去子

--- theories/Basis.v
+++ theories/Basis.v
@@ -9,10 +9,24 @@ Inductive empty : Type
   :=
   .
 
+Definition empty_elim_nodep (P : Type) (x : empty) : P
+  := match x with end .
+
+Definition empty_elim (P : empty -> Type) (x : empty) : P x
+  := match x with end .
+
 Inductive unit : Type
   :=
   | tt : unit
   .
+
+Definition unit_elim_nodep
+  (P : Type) (case_tt : P) (x : unit) : P
+  := match x with tt => case_tt end .
+
+Definition unit_elim
+  (P : unit -> Type) (case_tt : P tt) (x : unit) : P x
+  := match x with tt => case_tt end .
 
 Inductive sum (A : Type) (B : Type) : Type
   :=
@@ -20,18 +34,73 @@ Inductive sum (A : Type) (B : Type) : Type
   | right : B -> sum A B
   .
 
+Definition sum_elim_nodep
+  (A : Type) (B : Type) (P : Type)
+  (case_left : A -> P) (case_right : B -> P)
+  (x : sum A B) : P
+  := match x with
+    | left _ _ a => case_left a
+    | right _ _ b => case_right b
+  end .
+
+Definition sum_elim
+  (A : Type) (B : Type) (P : sum A B -> Type)
+  (case_left : forall a, P (left A B a))
+  (case_right : forall b, P (right A B b))
+  (x : sum A B) : P x
+  := match x with
+    | left _ _ a => case_left a
+    | right _ _ b => case_right b
+  end .
+
 Inductive prod (A : Type) (B : Type) : Type
   :=
   | pair : A -> B -> prod A B
   .
+
+Definition prod_elim_nodep
+  (A : Type) (B : Type) (P : Type)
+  (case_pair : A -> B -> P)
+  (x : prod A B) : P
+  := match x with pair _ _ a b => case_pair a b end .
+
+Definition prod_elim
+  (A : Type) (B : Type) (P : prod A B -> Type)
+  (case_pair : forall a b, P (pair A B a b))
+  (x : prod A B) : P x
+  := match x with pair _ _ a b => case_pair a b end .
 
 Inductive dsum (A : Type) (B : A -> Type) : Type
   :=
   | evi : forall x : A, B x -> dsum A B
   .
 
+Definition dsum_elim_nodep
+  (A : Type) (B : A -> Type) (P : Type)
+  (case_evi : forall x, B x -> P)
+  (x : dsum A B) : P
+  := match x with evi _ _ xv xH => case_evi xv xH end .
+
+Definition dsum_elim
+  (A : Type) (B : A -> Type) (P : dsum A B -> Type)
+  (case_evi : forall xv xH, P (evi A B xv xH))
+  (x : dsum A B) : P x
+  := match x with evi _ _ xv xH => case_evi xv xH end .
+
 Definition dprod (A : Type) (B : A -> Type) : Type
   := forall x : A, B x .
+
+Definition dprod_elim_nodep
+  (A : Type) (B : A -> Type) (P : Type)
+  (case : (forall x, B x) -> P)
+  (x : dprod A B) : P
+  := case x .
+
+Definition dprod_elim
+  (A : Type) (B : A -> Type) (P : dprod A B -> Type)
+  (case : forall f, P f)
+  (x : dprod A B) : P x
+  := case x .
 
 
 Module Categorical.
