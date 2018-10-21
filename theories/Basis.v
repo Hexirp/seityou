7b75e3753b1e3e2c61e592139a43ff5cf7515e5f
Hexirp <Hexirp@users.noreply.github.com>
2018-10-21 12:23:25 +0900

    Add: ほか色々

--- theories/Basis.v
+++ theories/Basis.v
@@ -174,6 +174,20 @@ Module Functional.
   Arguments paths_elim_nodep {_ _ _} _ {_} _ .
   Arguments paths_elim {_ _ _} _ {_} _ .
 
+  Definition idmap {A} : A -> A
+    := fun x => x .
+
+  Definition const {A B} : A -> B -> A
+    := fun x y => x .
+
+  Definition compose {A B C} : (B -> C) -> (A -> B) -> (A -> C)
+    := fun f g x => f (g x) .
+
+  Definition compose_dep
+    {A B C} (f : forall b, C b) (g : A -> B)
+    : forall a, C (g a)
+    := fun x => f (g x) .
+
   Definition absurd {A} : empty -> A
     := empty_elim_nodep .
 
@@ -183,11 +197,48 @@ Module Functional.
   Definition snd {A B} : prod A B -> B
     := prod_elim_nodep (fun _ b => b) .
 
+  Definition uncurry {A B C} : (A -> B -> C) -> (prod A B -> C)
+    := prod_elim_nodep .
+
   Definition dsum_fst {A B} : @dsum A B -> A
     := dsum_elim_nodep (fun xv _ => xv) .
 
   Definition dsum_snd {A B} (x : @dsum A B) : B (dsum_fst x)
-    := dsum_elim (P := fun x => B (dsum_fst x)) (fun _ xH => xH) x .
+    := dsum_elim (P := fun x' => B (dsum_fst x')) (fun _ xH => xH) x .
+
+  Definition paths_elim_nodep_nop
+    {A : Type} {P : A -> A -> Type}
+    (case_idpath : forall a, P a a)
+    (a a' : A) (x : paths a a') : P a a'
+    := paths_elim_nodep (case_idpath a) x .
+
+  Definition paths_elim_nop
+    {A : Type} {P : forall a a', paths a a' -> Type}
+    (case_idpath : forall a, P a a idpath)
+    (a a' : A) (x : paths a a') : P a a' x
+    := paths_elim (case_idpath a) x .
+
+  Definition inverse
+    {A : Type} (x y : A)
+    (p : paths x y) : paths y x
+    := paths_elim_nodep (P := fun x' => paths x' x) idpath p .
+
+  Definition concat
+    {A : Type} {x y z : A}
+    (p : paths x y) (q : paths y z) : paths x z
+    := paths_elim_nodep (paths_elim_nodep idpath p) q .
+
+  Definition transport
+    {A : Type} {P : A -> Type}
+    {x y : A} (p : paths x y)
+    (u : P x) : P y
+    := paths_elim_nodep u p .
+
+  Definition ap
+    {A B : Type} (f : A -> B)
+    {x y : A} (p : paths x y)
+    : paths (f x) (f y)
+    := paths_elim_nodep (P := fun x' => paths (f x) (f x')) idpath p .
 
 Module Functional.
 
