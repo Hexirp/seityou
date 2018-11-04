6527ec3ae2575faaa14f6b521f1f3082aa5673d0
Hexirp <Hexirp@users.noreply.github.com>
2018-11-04 14:28:00 +0900

    Add: なんちゃってWを

--- theories/W.v
+++ theories/W.v
@@ -2,11 +2,22 @@ Add LoadPath "./theories".
 
 Require Import Basis.
 
+Inductive w (A : Type) (B : A -> Type)
+  :=
+  | sup : forall x : A, (B x -> w A B) -> w A B
+  .
+
+Definition w_rect
+  (A : Type) (B : A -> Type) (P : w A B -> Type)
+  (case_sup : forall (x : A) (u : B x -> w A B), P (sup A B x u))
+  (x : w A B) : P x
+  := match x with sup _ _ x u => case_sup x u end .
+
 Axiom W : forall (A : Type) (B : A -> Type), Type.
 
-Axiom sup : forall (A : Type) (B : A -> Type) (x : A) (t : B x -> W A B), W A B.
+Axiom Sup : forall (A : Type) (B : A -> Type) (x : A) (t : B x -> W A B), W A B.
 
 Axiom wrec :
   forall (A : Type) (B : A -> Type) (C : W A B -> Type)
-  (c : forall (x : A) (u : B x -> W A B) (v : forall y : B x, C (u y)), C (sup A B x u))
+  (c : forall (x : A) (u : B x -> W A B) (v : forall y : B x, C (u y)), C (Sup A B x u))
   (w : W A B), C w .
