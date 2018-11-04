5cf17412860896ac7fd3783821f154a48b1e96aa
Hexirp <Hexirp@users.noreply.github.com>
2018-11-04 14:36:54 +0900

    Update: Wを高階帰納型スタイルで定義

--- theories/W.v
+++ theories/W.v
@@ -2,22 +2,26 @@ Add LoadPath "./theories".
 
 Require Import Basis.
 
-Inductive w (A : Type) (B : A -> Type)
+Private Inductive w (A : Type) (B : A -> Type)
   :=
   | sup : forall x : A, (B x -> w A B) -> w A B
   .
 
 Definition w_rect
   (A : Type) (B : A -> Type) (P : w A B -> Type)
-  (case_sup : forall (x : A) (u : B x -> w A B), P (sup A B x u))
+  (case_sup : forall x u, (forall y, P (u y)) -> P (sup A B x u))
   (x : w A B) : P x
-  := match x with sup _ _ x u => case_sup x u end .
+  .
+Proof.
+Admitted.
 
-Axiom W : forall (A : Type) (B : A -> Type), Type.
-
-Axiom Sup : forall (A : Type) (B : A -> Type) (x : A) (t : B x -> W A B), W A B.
-
-Axiom wrec :
-  forall (A : Type) (B : A -> Type) (C : W A B -> Type)
-  (c : forall (x : A) (u : B x -> W A B) (v : forall y : B x, C (u y)), C (Sup A B x u))
-  (w : W A B), C w .
+Definition w_rect_c
+  (A : Type) (B : A -> Type) (P : w A B -> Type)
+  (c : forall x u, (forall y, P (u y)) -> P (sup A B x u))
+  (x : A) (u : B x -> w A B)
+  : paths
+    (w_rect A B P c (sup A B x u))
+    (c x u (fun y => w_rect A B P c (u y)))
+  .
+Proof.
+Admitted.
