44b0fb948dcc663dae8b16ff336697dcafba127e
Hexirp <Hexirp@users.noreply.github.com>
2018-11-04 16:30:48 +0900

    Update: W-typeの意味
    http://www.lorentzcenter.nl/lc/web/2011/467/presentations/Presentation%20S%20Awodey.pdf
    https://homotopytypetheory.org/2012/01/19/inductive-types-in-hott/

--- theories/W.v
+++ theories/W.v
@@ -2,22 +2,33 @@ Add LoadPath "./theories".
 
 Require Import Basis.
 
-Polymorphic Cumulative Private Inductive w@{i j k} (A : Type@{i}) (B : A -> Type@{j}) : Type@{k}
+(** [A] に関して [B] で添え字付けられた整礎的な型。
+
+    [nat = 1 + nat] はよく知られているとおりである。
+    [F X = 1 + X] として [F X = X] となるような [X] が [nat] である。
+    さらに [tree = 1 + tree ^ 2] である。
+    すなわち [F X = 1 + X ^ 2] から生成される。
+    ここで、 [F X] を [X] の多項式として見なす。
+    すなわち [F X = X ^ a_0 + X ^ a_1 + ... + X ^ a_n] として一般化する。
+    すると、 [F X = Σ (k = 0 ... n) (X ^ a_k) である。
+    さらに一般化すると [F X = Σ (k : A) (X ^ B k)] となる。
+    最終的に [F X = dsum A (fun k : A => exp (B k) X)] となる。 *)
+Private Inductive w (A : Type) (B : A -> Type) : Type
   :=
   | sup : forall x : A, (B x -> w A B) -> w A B
   .
 
-Polymorphic Definition w_rect@{i j k l}
-  (A : Type@{i}) (B : A -> Type@{j}) (P : w@{i j k} A B -> Type@{l})
-  (case_sup : forall x u, (forall y, P (u y)) -> P (sup A B x u))
+Definition w_rect
+  (A : Type) (B : A -> Type) (P : w A B -> Type)
+  (case_sup : forall (x : A) (u : B x -> w A B), (forall y, P (u y)) -> P (sup A B x u))
   (x : w A B) : P x
   .
 Proof.
 Admitted.
 
-Polymorphic Definition w_rect_c@{i j k l}
-  (A : Type@{i}) (B : A -> Type@{j}) (P : w@{i j k} A B -> Type@{l})
-  (c : forall x u, (forall y, P (u y)) -> P (sup A B x u))
+Definition w_rect_c
+  (A : Type) (B : A -> Type) (P : w A B -> Type)
+  (c : forall (x : A) (u : B x -> w A B), (forall y, P (u y)) -> P (sup A B x u))
   (x : A) (u : B x -> w A B)
   : paths
     (w_rect A B P c (sup A B x u))
