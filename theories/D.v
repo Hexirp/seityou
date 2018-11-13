5c7906a8b2bbf2890f77377ee2e32ef69b0295d2
Hexirp <Hexirp@users.noreply.github.com>
2018-11-13 20:42:46 +0900

    Update: 訳する関数

--- theories/D.v
+++ theories/D.v
@@ -6,14 +6,39 @@ Declare ML Module "ltac_plugin".
 Export Set Default Proof Mode "Classic".
 
 
-Definition contradict (A : Type) (B : Type) : Type
+Definition contradict (A B : Type) : Type
   := A -> B -> empty .
 
-Definition contradict' (A : Type) (B : Type) : Type
+Definition contradict' (A B : Type) : Type
   := prod A B -> empty .
 
-Definition negate (A : Type) (B : Type) : Type
+Definition negate (A B : Type) : Type
   := A -> (B -> empty) .
 
-Definition negate' (A : Type) (B : Type) : Type
+Definition negate' (A B : Type) : Type
   := (A -> empty) -> B .
+
+
+Definition contradict_contradict' {A B : Type}
+  : contradict A B -> contradict' A B
+  := uncurry .
+
+Definition contradict_negate {A B : Type}
+  : contradict A B -> negate A B
+  := idmap .
+
+Definition contradict'_contradict {A B : Type}
+  : contradict' A B -> contradict A B
+  := fun f a b => f (pair a b) .
+
+Definition contradict'_negate {A B : Type}
+  : contradict' A B -> negate A B
+  := fun f a b => f (pair a b) .
+
+Definition negate_contradict {A B : Type}
+  : negate A B -> contradict A B
+  := idmap .
+
+Definition negate_contradict' {A B : Type}
+  : negate A B -> contradict' A B
+  := uncurry .
