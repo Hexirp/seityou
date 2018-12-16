c192e4e2c71f95dc50cf0deaf28160d25ce84a63
Hexirp <Hexirp@users.noreply.github.com>
2018-12-16 12:54:08 +0900

    Update: Existential variable を指定して埋めたい

--- theories/Equivalence.v
+++ theories/Equivalence.v
@@ -15,4 +15,5 @@ Set Default Proof Mode "Classic".
 Definition is_equiv_idmap {A : Type} : is_equiv (@idmap A) .
 Proof.
  refine (dpair idmap _) .
- refine (dpair ?retr _) .
+ refine (dpair _ _) .
+ refine (let ?retl := _ in _) .
