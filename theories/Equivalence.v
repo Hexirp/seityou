5dd76a1ea1a4cdceebc69221117cb54ae9822a92
Hexirp <Hexirp@users.noreply.github.com>
2018-12-16 12:49:49 +0900

    Add: Equivalence.v

---
+++ theories/Equivalence.v
@@ -0,0 +1,19 @@
+(** * Equivalence
+
+    等価性に関する定理や定義。 *)
+
+
+Require Export Homotopy.
+Require Import Path.
+
+
+Declare ML Module "ltac_plugin".
+
+Set Default Proof Mode "Classic".
+
+
+Definition is_equiv_idmap {A : Type} : is_equiv (@idmap A) .
+Proof.
+ refine (dpair idmap _) .
+ refine (dpair _ _) .
+ 
