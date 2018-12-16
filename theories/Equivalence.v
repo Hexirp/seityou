4b17493564f9c388fb5f074015988f9af8d84a75
Hexirp <Hexirp@users.noreply.github.com>
2018-12-16 13:13:54 +0900

    Add: is_equiv_compose

--- theories/Equivalence.v
+++ theories/Equivalence.v
@@ -19,3 +19,17 @@ Proof.
  refine (dpair (fun x => idpath x) _) .
  exact (fun x => idpath (idpath x)) .
 Defined.
+
+Definition equiv_idmap {A : Type} : equiv A A .
+Proof.
+ refine (dpair idmap _) .
+ exact is_equiv_idmap .
+Defined.
+
+Definition is_equiv_compose
+  {A B C : Type} {f : A -> B} {g : B -> C}
+  (f_iv : is_equiv f) (g_iv : is_equiv g)
+  : is_equiv (compose g f) .
+Proof.
+ refine (dpair (compose (equiv_inv f_iv) (equiv_inv g_iv)) _) .
+ 
