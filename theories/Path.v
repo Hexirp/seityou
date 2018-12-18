8529244e0ea7487351e5fd156d8902c52330877c
Hexirp <Hexirp@users.noreply.github.com>
2018-12-18 20:39:30 +0900

    Update: Homotopy から Path へ基本的なものを移動

--- theories/Path.v
+++ theories/Path.v
@@ -50,27 +50,134 @@ End Notation.
 Import Notation.
 
 
-(** ** Unit *)
+(** ** Application *)
 
-(** 全ての [unit] は [tt] に等しい。 *)
-Definition path_unit : forall x, tt = x .
-Proof.
- refine (unit_elim _) .
- exact 1 .
-Defined.
+(** [ap] のバリエーションは以下の命令規則に従う。
 
-(** ** Empty *)
+    [apNK] は関数のK次道を値のN次道に適用する。関数が依存版である場合は、
+    [apNK_dep] となる。関数がLつの値を取るとき、値のN次道はLつあり、名前は
+    [apNK_L] となる。
 
-(** 全ての [empty] はたがいに等しい。 *)
-Definition path_empty : forall x y : empty, x = y .
-Proof.
- exact empty_elim .
-Defined.
+    K次道は値、値の道、値の道の道といったものを表す。これは縮小 (Truncation)
+    にも関わる表記法である。型 [A] に対して、そのK次道は、
 
-(** ** Sum *)
+    * 0-path: A
+    * 1-path: paths A x y
+    * 2-path: paths (paths A x y) p q
+    * 3-path: paths (paths (paths A x y) p q) r s
 
-(** *)
+    このようなことである。 *)
 
+(** [f] を [x] に適用する。 *)
+Definition ap00
+  {A B : Type}
+  (f : A -> B)
+  (x : A)
+  : B
+  := apply f x .
+
+(** [f] を [p] に適用する。
+
+    [ap] の別名。 *)
+Definition ap01
+  {A B : Type}
+  (f : A -> B)
+  {x y : A} (p : x = y)
+  : f x = f y
+  := ap f p .
+
+(** [p] を [x] に適用する。 *)
+Definition ap10
+  {A B : Type}
+  {f g : A -> B} (p : f = g)
+  (x : A)
+  : f x = g x
+  := paths_elim_nodep (P := fun g' => f x = g' x) 1 p .
+
+(** [ap10] の依存版。 *)
+Definition ap10_dep
+  {A : Type} {B : A -> Type}
+  {f g : forall a, B a} (p : f = g)
+  (x : A)
+  : f x = g x
+  := paths_elim_nodep (P := fun g' => f x = g' x) 1 p .
+
+(** [p] を [q] に適用する。 *)
+Definition ap11
+  {A B : Type}
+  {f g : A -> B} (p : f = g)
+  {x y : A} (q : x = y)
+  : f x = g y
+  := paths_elim_nodep (P := fun g' => f x = g' y) (ap f q) p .
+
+(** [f] を [p] と [q] に適用する。 *)
+Definition ap01_2
+  {A B C : Type}
+  (f : A -> B -> C)
+  (x y : A) (p : x = y)
+  (z w : B) (q : z = w)
+  : f x z = f y w
+  := ap11 (ap f p) q .
+
+(** [ap] の依存版。
+
+    返される型の [paths (transport p (f x)) (f y)] という形は「依存道」と
+    呼ばれる。 [f x] と [f y] はそれぞれ [P x] と [P y] という型を持っている
+    ため、そのまま [paths (f x) (f y)] と書くことはできない。そこで [f x] を
+    [p] によって輸送することで両辺の型を合わせている。依存型に対する道。
+    [p] に依存する道。詳細は等式型のバリエーションを集めた Equality に
+    譲る。 *)
+Definition ap_dep
+  {A : Type} {B : A -> Type}
+  (f : forall a, B a)
+  {x y : A} (p : x = y)
+  : p # f x = f y
+  := paths_elim (P := fun y' p' => p' # f x = f y') idpath p .
+
+(** [ap01] の依存版。
+
+    [ap_dep] の別名。 *)
+Definition ap01_dep
+  {A : Type} {B : A -> Type}
+  (f : forall a, B a)
+  {x y : A} (p : x = y)
+  : p # f x = f y
+  := ap_dep f p .
+
+
+(** ** Pointwise paths *)
+
+(** 点ごとの道。関数の外延性等値性。
+
+    名前は "pointwize paths" を縮めたもの。 *)
+Definition pwpaths
+  {A : Type} {B : A -> Type} (f g : forall a, B a) : Type
+  := forall a, paths (f a) (g a) .
+
+(** 道から、点ごとの道を得る。
+
+    逆は [funext] であり、証明も反証もできない。 *)
+Definition pwpaths_paths
+  {A : Type} {B : A -> Type}
+  {f g : forall a, B a} (p : paths f g)
+  : pwpaths f g
+  := ap10_dep p .
+
+(** 点ごとの道の両辺に、左から関数を合成する。 *)
+Definition pwpaths_compose01
+  {A B C : Type}
+  (f : B -> C)
+  {g h : A -> B} (p : pwpaths g h)
+  : pwpaths (compose f g) (compose f h)
+  := compose01 (fun a => ap (x := g a) (y := h a) f) p .
+
+(** 点ごとの道の両辺に、右から関数を合成する。 *)
+Definition pwpaths_compose10
+  {A B C : Type}
+  {f g : B -> C} (p : pwpaths f g)
+  (h : A -> B)
+  : pwpaths (compose f h) (compose g h)
+  := compose10 p h .
 
 (** [p] を反転して [q] を合成する。
 
