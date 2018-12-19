1ab384e29507e44a96dcc535984ab9be0a139f02
Hexirp <Hexirp@users.noreply.github.com>
2018-12-19 20:30:01 +0900

    Update: Contraction を記法と

--- theories/Contraction.v
+++ theories/Contraction.v
@@ -2,19 +2,19 @@
 
     可縮性に関する定理や定義。 *)
 
-
 Require Export Homotopy.
-Require Import Path.
 
+(** 戦術を使う。 *)
+Declare ML Module "ltac_plugin" .
+Set Default Proof Mode "Classic" .
 
-Declare ML Module "ltac_plugin".
-
-Set Default Proof Mode "Classic".
+(** 記法を使う。 *)
+Import Homotopy.Notation .
 
 
 (** [A] が [contr] であれば [x y : A] の間に道がある。 *)
 Definition path_contr
-  {A : Type} (IC : contr A) (x y : A) : paths x y .
+  {A : Type} (IC : contr A) (x y : A) : x = y .
 Proof.
  refine (coninv (y := dfst IC) _ _) .
  -
@@ -25,20 +25,17 @@ Defined.
 
 (** [A] が [IC : contr A] であれば、その二点の間の道 [p : paths x y] は
     [path_contr IC x y] からの道を持つ。 *)
-Lemma K_path_contr
-  {A : Type} (IC : contr A) {x y : A} (p : paths x y)
-  : paths (path_contr IC x y) p .
+Definition K_path_contr
+  {A : Type} (IC : contr A) {x y : A} (p : x = y)
+  : path_contr IC x y = p .
 Proof.
- refine (
-   paths_elim _ p
-     (P := fun y' p' => paths (path_contr IC x y') p')
-   ) .
+ refine (paths_elim (P := fun y' p' => path_contr IC x y' = p') _ p) .
  exact (coninv_pp (dsnd IC x)) .
 Defined.
 
 (** [A] が [contr] であれば [p q : paths x y] の間に道がある。 *)
 Definition path_path_contr
-  {A : Type} (IC : contr A) {x y : A} (p q : paths x y) : paths p q .
+  {A : Type} (IC : contr A) {x y : A} (p q : x = y) : p = q .
 Proof.
  refine (coninv (y := path_contr IC x y) _ _) .
  -
@@ -49,7 +46,7 @@ Defined.
 
 (** [A] が [contr] であれば、その二点の間の [paths] も [contr] である。 *)
 Definition contr_paths_contr
-  {A : Type} (IC : contr A) (x y : A) : contr (paths x y) .
+  {A : Type} (IC : contr A) (x y : A) : contr (x = y) .
 Proof.
  refine (dpair (path_contr IC x y) _) .
  exact (K_path_contr IC) .
@@ -57,23 +54,23 @@ Defined.
 
 
 (** [x] を始点とする道の集まり。 *)
-Definition based_paths {X : Type} (x : X) : Type := dsum (paths x) .
+Definition based_paths {X : Type} (x : X) : Type := sigma y, x = y .
 
 (** [p : based_paths x] は [dpair x idpath] からの道を持つ。 *)
 Definition path_based_paths
   {X : Type} {x : X} (p : based_paths x)
-  : paths (dpair x idpath) p .
+  : dpair x 1 = p .
 Proof.
  refine (dsum_elim _ p) .
  refine (@paths_elim X x _ _) .
- exact idpath .
+ exact 1 .
 Defined.
 
 (** [based_paths] は [contr] である。 *)
 Definition contr_based_paths
   {X : Type} (x : X) : contr (based_paths x) .
 Proof.
- refine (dpair (dpair x idpath) _) .
+ refine (dpair (dpair x 1) _) .
  exact path_based_paths .
 Defined.
 
@@ -91,7 +88,7 @@ Defined.
 (** [paths_elim] を [based_paths] を使って書き直したもの。 *)
 Definition paths_elim_by_based_paths
   {A : Type} (a : A) (P : based_paths a -> Type)
-  (c : P (dpair a idpath))
+  (c : P (dpair a 1))
   (x : based_paths a) : P x .
 Proof.
  revert x .
@@ -105,7 +102,7 @@ Defined.
     命題的定値 (propositionally constant) である。 *)
 Definition contr_dom_constant
   {A B : Type} (IC : contr A) (f : A -> B) {x y : A}
-  : paths (f x) (f y) .
+  : f x = f y .
 Proof.
  refine (ap f _) .
  exact (path_contr IC x y) .
