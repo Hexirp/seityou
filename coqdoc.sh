7124b57430e68fb1d9fe22ea5bd363b4f4ef922b
Hexirp <Hexirp@users.noreply.github.com>
2018-12-31 15:57:02 +0900

    Add: coqdoc.sh

---
+++ coqdoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Hexirpの環境に固有の設定であるが、めんどくさいため消さない
+COQ_PATH=/c/coq
+
+PATH=$COQ_PATH/bin:$PATH
+
+coqdoc \
+ -R theories/ Seityou \
+ theories/Basis.v \
+ theories/Path.v \
+ theories/Homotopy.v \
+ theories/Pwpath.v \
+ theories/Contraction.v \
+ theories/Equivalence.v
