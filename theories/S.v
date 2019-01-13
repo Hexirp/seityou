a6ea4b2ef9be277087e66c259a2f8115415099ae
Hexirp <Hexirp@users.noreply.github.com>
2019-01-13 21:03:43 +0900

    Fix: 相互再帰の固定されたパラメーターは統一せよ
    Parameters should be syntactically the same for each inductive type.

--- theories/S.v
+++ theories/S.v
@@ -4,8 +4,8 @@ Inductive sorted_list {A : Type} (R : A -> A -> Type)
   :=
   | s_nil : sorted_list R
   | s_cons : forall (x : A) (xs : sorted_list R), forall_le R x xs -> sorted_list R
-with forall_le {A : Type} (R : A -> A -> Type) (x : A) : sorted_list R -> Type
+with forall_le {A : Type} (R : A -> A -> Type) : A -> sorted_list R -> Type
   :=
-  | f_nil : forall_le R x (s_nil R)
-  | f_cons : forall (y : A) (ys : sorted_list R) (yH : forall_le R y ys), R x y -> forall_le R x ys -> forall_le R x (s_cons R y ys yH)
+  | f_nil : forall x : A, forall_le R x (s_nil R)
+  | f_cons : forall (x : A) (y : A) (ys : sorted_list R) (yH : forall_le R y ys), R x y -> forall_le R x ys -> forall_le R x (s_cons R y ys yH)
 .
