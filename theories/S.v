7d491984c9123a80eb4b8f0a5db13cc3ad9eed54
Hexirp <Hexirp@users.noreply.github.com>
2019-01-13 21:38:53 +0900

    Update: これは無理

--- theories/S.v
+++ theories/S.v
@@ -3,5 +3,9 @@ Require Import Basis .
 Inductive sorted_list {A : Type} (R : A -> A -> Type) (x : A) : Type
   :=
   | s_nil : sorted_list R x
-  | s_cons : forall (y : A) (xs : sorted_list R y), R x y -> sorted_list R x
+  | s_cons : forall (y : A) (xs : sorted_list R y), forall_le R x y xs -> sorted_list R x
+with forall_le {A : Type} (R : A -> A -> Type) (x : A) : forall y : A, sorted_list y -> Type
+  :=
+  | f_nil : forall y : A, R x y -> forall_le R x y (s_nil R y)
+  | f_cons : forall (y z : A) (xs : sorted_list R z), forall_le R y z 
   .
