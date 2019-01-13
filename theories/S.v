d1927692260b6d944957cafabff7de61b5336c96
Hexirp <Hexirp@users.noreply.github.com>
2019-01-13 21:31:57 +0900

    Update: myuon さんの提案
    https://twitter.com/myuon_myon/status/1084423275906658304
    
    訂正: 型でやるならsorted_list aをaから始まるsorted_listとして定義したらs_consをforall x. forall ys:sorted_list a. x <= a -> sorted xとかでできません？

--- theories/S.v
+++ theories/S.v
@@ -1,11 +1,7 @@
 Require Import Basis .
 
-Inductive sorted_list {A : Type} (R : A -> A -> Type)
+Inductive sorted_list {A : Type} (R : A -> A -> Type) (x : A) : Type
   :=
-  | s_nil : sorted_list R
-  | s_cons : forall (x : A) (xs : sorted_list R), forall_le R x xs -> sorted_list R
-with forall_le {A : Type} (R : A -> A -> Type) : A -> sorted_list R -> Type
-  :=
-  | f_nil : forall x : A, forall_le R x (s_nil R)
-  | f_cons : forall (x : A) (y : A) (ys : sorted_list R) (yH : forall_le R y ys), R x y -> forall_le R x ys -> forall_le R x (s_cons R y ys yH)
-.
+  | s_nil : sorted_list R x
+  | s_cons : forall (y : A) (xs : sorted_list R y), R x y -> sorted_list R x
+  .
