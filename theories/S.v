9df184a2577967f65da7b1daaeedc2c158f40b07
Hexirp <Hexirp@users.noreply.github.com>
2019-01-14 10:54:47 +0900

    Add: acc

--- theories/S.v
+++ theories/S.v
@@ -1,5 +1,6 @@
 Require Import Basis .
 
+
 Inductive list (A : Type) : Type
   :=
   | nil : list A
@@ -26,3 +27,22 @@ Inductive sorted_list (A : Type) (R : A -> A -> Type) : Type
   :=
   | mk_sl : forall x : list A, is_sorted A R x -> sorted_list A R
   .
+
+
+Inductive acc (A : Type) (R : A -> A -> Type) (x : A) : Type
+  :=
+  | mk_acc : (forall xp : A, R xp x -> acc A R xp) -> acc A R x
+  .
+
+Definition acc_case
+  (A : Type) (R : A -> A -> Type) (x : A) (P : acc A R x -> Type)
+  (case_mk_acc : forall Hp : forall xp : A, R xp x -> acc A R xp, P (mk_acc A R x Hp))
+  (H : acc A R x) : P H
+  := match H with mk_acc _ _ _ Hp => case_mk_acc Hp end .
+
+Definition acc_rec
+  (A : Type) (R : A -> A -> Type) (P : A -> Type)
+  (case_mk_acc : forall x : A, (forall xp : A, R xp x -> P xp) -> P x)
+  (x : A) (H : acc A R x) : P x .
+Proof.
+Admitted.
