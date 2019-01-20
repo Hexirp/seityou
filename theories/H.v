f97c3409c47b182eaffb48ac2acdc11a48cb9978
Hexirp <Hexirp@users.noreply.github.com>
2019-01-20 15:38:16 +0900

    Add: 存在変数で困る例
    ここで、?Goal と ?Goal0 が現れている。存在変数はゴールが別のゴールに
    依存する場合に現れる。ここを先にタクティックで解きたいのだが、
    飛ばされてしまう……

---
+++ theories/H.v
@@ -0,0 +1,26 @@
+Require Import Basis .
+
+Import Basis.Notation .
+
+
+Declare ML Module "ltac_plugin" .
+Set Default Proof Mode "Classic" .
+
+
+Axiom lsum : Type -> Type -> Type .
+
+Axiom lsum_left : forall A B, A -> lsum A B .
+Axiom lsum_right : forall A B, B -> lsum A B .
+
+Axiom lsum_elim_nodep
+  : forall A B P (cl : A -> P) (cr : B -> P),
+    (forall a b, cl a = cr b) ->
+    lsum A B -> P .
+
+Definition wk_dec (P : Type) := lsum (unit -> P) (P -> empty) .
+
+Definition collapse (P : Type) (wdec : wk_dec P) : P -> P .
+Proof.
+ revert wdec .
+ refine (lsum_elim_nodep (unit -> P) (P -> empty) (P -> P) _ _ _) .
+Admitted.
