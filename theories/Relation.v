ea4030421590a511034ea0b67296922b86d353fe
Hexirp <Hexirp@users.noreply.github.com>
2019-01-29 18:25:55 +0900

    Add: order_preserving

--- theories/Relation.v
+++ theories/Relation.v
@@ -91,6 +91,14 @@ Proof.
 Defined.
 
 
+(** 順序を保つ (order-preserving) 関数である。 *)
+Definition order_preserving
+  {A : Type} (R : A -> A -> Type)
+  {B : Type} (S : B -> B -> Type)
+  (f : A -> B) : Type
+  := forall x y : A, R x y -> S (f x) (f y) .
+
+
 (** ** Well foundness *)
 
 (** [R] は整礎である。 *)
