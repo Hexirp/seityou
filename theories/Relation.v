43dd10d0d10532e1e2591f806558cc5480ab5e98
Hexirp <Hexirp@users.noreply.github.com>
2019-01-29 15:02:42 +0900

    Update: こうしたほうがいい

--- theories/Relation.v
+++ theories/Relation.v
@@ -12,32 +12,32 @@ Set Default Proof Mode "Classic" .
 Import Basis.Notation .
 
 
-(** [A] の関係 [R : A -> A -> Type] は [x : A] において整礎である。 *)
-Inductive acc {A : Type} (R : A -> A -> Type) (x : A) : Type
+(** [A] の関係 [R] は [x : A] において整礎である。 *)
+Inductive acc (A : Type) (R : A -> A -> Type) (x : A) : Type
   :=
-  | mk_acc : (forall xp : A, R xp x -> acc R xp) -> acc R x
+  | mk_acc : (forall xp : A, R xp x -> acc A R xp) -> acc A R x
   .
 
 Definition acc_case_nodep
-  {A : Type} {R : A -> A -> Type} {x : A} {P : Type}
-  (case_mk_acc : (forall xp : A, R xp x -> acc R xp) -> P)
-  (H : acc R x) : P
-  := match H with mk_acc _ _ Hp => case_mk_acc Hp end .
+  (A : Type) (R : A -> A -> Type) (x : A) (P : Type)
+  (case_mk_acc : (forall xp : A, R xp x -> acc A R xp) -> P)
+  (H : acc A R x) : P
+  := match H with mk_acc _ _ _ Hp => case_mk_acc Hp end .
 
 Definition acc_case
-  {A : Type} {R : A -> A -> Type} {x : A} {P : acc R x -> Type}
-  (case_mk_acc : forall Hp : forall xp : A, R xp x -> acc R xp, P (mk_acc R x Hp))
-  (H : acc R x) : P H
-  := match H with mk_acc _ _ Hp => case_mk_acc Hp end .
+  (A : Type) (R : A -> A -> Type) (x : A) (P : acc A R x -> Type)
+  (case_mk_acc : forall Hp : forall xp : A, R xp x -> acc A R xp, P (mk_acc A R x Hp))
+  (H : acc A R x) : P H
+  := match H with mk_acc _ _ _ Hp => case_mk_acc Hp end .
 
 Definition acc_rec
   (A : Type) (R : A -> A -> Type) (P : A -> Type)
   (case_mk_acc : forall x : A, (forall xp : A, R xp x -> P xp) -> P x)
-  (x : A) (H : acc R x) : P x .
+  (x : A) (H : acc A R x) : P x .
 Proof.
  revert x H .
- refine (fix go (x : A) (H : acc R x) {struct H} : P x := _) .
- refine (acc_case_nodep _ H) .
+ refine (fix go (x : A) (H : acc A R x) {struct H} : P x := _) .
+ refine (acc_case_nodep A R x (P x) _ H) .
  -
   refine (fun Hp => _) .
   refine (case_mk_acc x _) .
@@ -46,18 +46,25 @@ Proof.
 Defined.
 
 Definition acc_rect
-  (A : Type) (R : A -> A -> Type) (P : forall x : A, acc R x -> Type)
+  (A : Type) (R : A -> A -> Type) (P : forall x : A, acc A R x -> Type)
   (case_mk_acc
-          : forall (x : A) (Hp : forall xp : A, R xp x -> acc R xp),
-              (forall (xp : A) (xpR : R xp x), P xp (Hp xp xpR)) -> P x (mk_acc R x Hp))
-  (x : A) (H : acc R x) : P x H .
+          : forall (x : A) (Hp : forall xp : A, R xp x -> acc A R xp),
+              (forall (xp : A) (xpR : R xp x), P xp (Hp xp xpR)) -> P x (mk_acc A R x Hp))
+  (x : A) (H : acc A R x) : P x H .
 Proof.
  revert x H .
- refine (fix go (x : A) (H : acc R x) {struct H} : P x H := _) .
- refine (acc_case _ H) .
+ refine (fix go (x : A) (H : acc A R x) {struct H} : P x H := _) .
+ refine (acc_case A R x (P x) _ H) .
  -
   refine (fun Hp => _) .
   refine (case_mk_acc x Hp _) .
   refine (fun xp xpR => _) .
   exact (go xp (Hp xp xpR)) .
 Defined.
+
+Arguments acc {_} _ _ .
+Arguments mk_acc {_ _ _} _ .
+Arguments acc_case_nodep {_ _ _ _} _ _ .
+Arguments acc_case {_ _ _ _} _ _ .
+Arguments acc_rec {_ _ _} _ {_} _ .
+Arguments acc_rect {_ _ _} _ {_} _ .
