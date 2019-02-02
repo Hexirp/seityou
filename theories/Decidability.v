c4edbd536aa2d78c3360921d4566b32456f96938
Hexirp <Hexirp@users.noreply.github.com>
2019-02-02 12:53:08 +0900

    Add: Decidability の参考文献

--- theories/Decidability.v
+++ theories/Decidability.v
@@ -51,3 +51,10 @@ Proof.
  refine (left _) .
  exact (center ic) .
 Defined.
+
+
+(** 参考文献:
+
+    * https://github.com/HoTT/HoTT/blob/1940297dd121d54d033274d84c5d023fdc56bfb4/theories/Basics/Decidable.v
+
+    *)
