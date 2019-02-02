4f0eb96053fcac0ddd4c7e24a93d62608f06d591
Hexirp <Hexirp@users.noreply.github.com>
2019-02-02 12:45:29 +0900

    Add: Path の参考文献

--- theories/Path.v
+++ theories/Path.v
@@ -254,3 +254,10 @@ Module Notation .
     .
 
 End Notation .
+
+(** 参考文献:
+
+    * https://github.com/HoTT/HoTT/blob/1940297dd121d54d033274d84c5d023fdc56bfb4/theories/Basics/Overture.v
+    * https://github.com/HoTT/HoTT/blob/1940297dd121d54d033274d84c5d023fdc56bfb4/theories/Basics/PathGroupoids.v
+
+    *)
