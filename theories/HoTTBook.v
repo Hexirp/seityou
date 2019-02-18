6ffba2654a058a909d1694e1f4607d72461557d9
Hexirp <Hexirp@users.noreply.github.com>
2019-02-18 19:42:16 +0900

    Add: HoTTBook

---
+++ theories/HoTTBook.v
@@ -0,0 +1,26 @@
+Require Import Basis .
+
+(** 戦術を使う。 *)
+Declare ML Module "ltac_plugin" .
+Set Default Proof Mode "Classic" .
+
+(** 記法を使う。 *)
+Import Basis.Notation .
+Import Basis.Notation.Path .
+
+
+Arguments paths : clear implicits .
+Arguments idpath : clear implicits .
+Arguments paths_elim_nop : clear implicits .
+
+Definition exerise_1_8
+  (A : Type) (a : A) (P : forall a', paths A a a' -> Type)
+  (case_idpath : P a (idpath A a))
+  (a' : A) (x : paths A a a') : P a' x .
+Proof.
+ revert x P case_idpath .
+ revert a a' .
+ refine (paths_elim_nop A ?[ex_P] _) .
+ refine (fun a P h => _) .
+ exact h .
+Defined.
