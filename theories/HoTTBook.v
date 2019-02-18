b7fe596f06b1a34ca5540d46a9c4e26b093edb91
Hexirp <Hexirp@users.noreply.github.com>
2019-02-18 19:52:30 +0900

    Update: book_1_12_2_a を真面目にやる

--- theories/HoTTBook.v
+++ theories/HoTTBook.v
@@ -11,9 +11,18 @@ Import Basis.Notation.Path .
 
 Arguments paths : clear implicits .
 Arguments idpath : clear implicits .
+Arguments paths_elim : clear implicits .
 Arguments paths_elim_nop : clear implicits .
 
-Definition book_1_12_2_a := paths_elim_nop .
+Definition book_1_12_2_a
+  {A : Type} {P : forall a a', paths A a a' -> Type}
+  (case_idpath : forall a, P a a (idpath A a))
+  (a a' : A) (x : paths A a a') : P a a' x .
+Proof.
+ revert a' x .
+ refine (paths_elim A a (P a) _) .
+ exact (case_idpath a) .
+Defined.
 
 Definition book_1_12_2_b
   (A : Type) (a : A) (P : forall a', paths A a a' -> Type)
